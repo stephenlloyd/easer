ed27cbb093f9cb8182d5b96c24095518eacb6d85
unknown <stephen.lloyd@.uswitch.com>
2010-06-25 15:47:55 +0100

    broken code around tools and calculating bmi

--- app/controllers/tools_controller.rb
+++ app/controllers/tools_controller.rb
@@ -2,7 +2,7 @@ class ToolsController < ApplicationController
   require 'bmi'
 
   def calculate
-		
+	@bmi = Bmi.calc_bmi(:weight => params[:toolsform][:weight_lb], :height => params[:toolsform][:height_cm])
   end
 
 end
