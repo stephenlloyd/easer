1d393fccf1575c824b6b22857f8c49e52d06685f
unknown <stephen.lloyd@.uswitch.com>
2010-06-25 11:37:26 +0100

    made a home page

--- config/routes.rb
+++ config/routes.rb
@@ -42,5 +42,5 @@ ActionController::Routing::Routes.draw do |map|
   # consider removing or commenting them out if you're using named routes and resources.
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
-  map.root :controller => "posts"
+  map.root :controller => "index"
 end
