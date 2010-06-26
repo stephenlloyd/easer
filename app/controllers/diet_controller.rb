b996c4ca622b12dd563b73fc2d93e924d3404fec
unknown <Steve@.(none)>
2010-06-26 11:58:07 +0100

    more progress... added javascript chart and added bmr to my diets

--- app/controllers/diet_controller.rb
+++ app/controllers/diet_controller.rb
@@ -1,7 +1,13 @@
 class DietController < ApplicationController
+  require 'bmr'
 
   def mydiet
-
+    if !session[:user_id]
+      redirect_to :controller=>'user', :action=> 'login'
+      return
+    end
+        @bmr = Bmr.new()
+        @bmr = @bmr.calc_bmr_for_session(session[:user_id])
   end
 
 end
--- app/controllers/tools_controller.rb
+++ app/controllers/tools_controller.rb
@@ -4,7 +4,7 @@ class ToolsController < ApplicationController
   def calculate
     @weight = params[:toolsform][:weight_lb].to_f
     @height = params[:toolsform][:height_cm].to_f
-	@bmi = Bmi.calc_bmi(@weight, @height)
+	session[:bmi] = Bmi.calc_bmi(@weight, @height)
     redirect_to :action=>''
   end
 
